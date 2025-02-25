41383ed7b0f1799a7edabe268ba9f282e17dfb41
ankaikai <ankaikai@stu.pku.edu.cn>
2025-02-25 14:42:26 +0800

    train

---
+++ Training/llama31_8b_dpo_full.py
@@ -0,0 +1,249 @@
+# Copyright (c) OpenMMLab. All rights reserved.
+import torch
+from datasets import load_dataset, load_from_disk
+from mmengine.dataset import DefaultSampler
+from mmengine.hooks import (CheckpointHook, DistSamplerSeedHook, IterTimerHook,
+                            LoggerHook, ParamSchedulerHook)
+from mmengine.optim import AmpOptimWrapper, CosineAnnealingLR, LinearLR
+from peft import LoraConfig
+from torch.optim import AdamW
+from transformers import (AutoModelForCausalLM, AutoTokenizer,
+                          BitsAndBytesConfig)
+
+# from xtuner.dataset import process_hf_dataset
+# from xtuner.dataset.collate_fns import default_collate_fn
+from xtuner.dataset.collate_fns.preference_collate_fn import preference_collate_fn
+from xtuner.dataset import preference_dataset
+from xtuner.dataset.preference_dataset import build_preference_dataset
+from xtuner.engine.hooks import (DatasetInfoHook, EvaluateChatHook, HFCheckpointHook,
+                                 VarlenAttnArgsToMessageHubHook)
+from xtuner.engine.runner import TrainLoop
+# from xtuner.model import SupervisedFinetune
+from xtuner.model.dpo import DPO
+from xtuner.parallel.sequence import SequenceParallelSampler
+from xtuner.utils import PROMPT_TEMPLATE, SYSTEM_TEMPLATE
+from mmengine.visualization import Visualizer, WandbVisBackend
+
+
+
+#######################################################################
+#                          PART 1  Settings                           #
+#######################################################################
+pretrained_model_name_or_path = 'your_sft_model'
+
+
+dpo_loss_type = 'sigmoid'  # One of ['sigmoid', 'nca_pair']
+use_varlen_attn = True
+# Model
+loss_beta = 0.1     # change from 0.1 to 0.3
+label_smoothing = 0.0
+
+# Evaluate the generation performance during the training
+evaluation_freq = 100
+
+# Data 
+data_path = 'your_data'
+prompt_template = PROMPT_TEMPLATE.llama3_chat
+max_length = 2048*4
+pack_to_max_length = True
+
+# Save
+save_steps = 10000
+save_total_limit = 1  # Maximum checkpoints to keep (-1 means unlimited)
+out_dir = "your_path"
+
+# parallel
+sequence_parallel_size = 1
+
+# Scheduler & Optimizer
+batch_size = 1  # per_device
+accumulative_counts = 1
+accumulative_counts *= sequence_parallel_size
+dataloader_num_workers = 0
+max_epochs = 3
+optim_type = AdamW
+lr = 5e-7
+betas = (0.9, 0.999)
+weight_decay = 0
+max_norm = 1  # grad clip
+warmup_ratio = 0.03
+
+
+#######################################################################
+#                      PART 2  Model & Tokenizer                      #
+#######################################################################
+tokenizer = dict(
+    type=AutoTokenizer.from_pretrained,
+    pretrained_model_name_or_path=pretrained_model_name_or_path,
+    trust_remote_code=True,
+    padding_side='right')
+
+model = dict(
+    type=DPO,
+    use_varlen_attn=use_varlen_attn,
+    loss_type=dpo_loss_type,
+    beta=loss_beta,
+    label_smoothing=label_smoothing,
+    llm=dict(
+        type=AutoModelForCausalLM.from_pretrained,
+        pretrained_model_name_or_path=pretrained_model_name_or_path,
+        trust_remote_code=True,
+        torch_dtype=torch.float16,
+        ),
+    ref_llm=dict(
+        type=AutoModelForCausalLM.from_pretrained,
+        pretrained_model_name_or_path=pretrained_model_name_or_path,
+        trust_remote_code=True,
+        torch_dtype=torch.float16,
+        ),
+)
+
+
+
+#######################################################################
+#                      PART 3  Dataset & Dataloader                   #
+#######################################################################
+
+def sharegpt_dpo_map_fn(example):
+    conversation = []
+    if "history" in example:
+        history = example["history"]
+        for i in range(0, len(history), 2):
+            conversation.append({"role": "user", "content": history[i]["value"]})
+            conversation.append({"role": "assistant", "content": history[i+1]["value"]})
+    conversation.append({"role": "user", "content": example["prompt"]})
+
+    return {'prompt': conversation, 'chosen': [{"content": example["chosen"], "role": "assistant"}], 'rejected': [{"content": example["rejected"], "role": "assistant"}]}
+
+
+
+sampler = SequenceParallelSampler \
+    if sequence_parallel_size > 1 else DefaultSampler
+
+train_dataset = dict(
+    type=build_preference_dataset,
+    dataset=dict(type=load_from_disk, dataset_path=data_path),
+    tokenizer=tokenizer,
+    max_length=max_length,
+    dataset_map_fn=sharegpt_dpo_map_fn,
+    is_dpo=True,
+    is_reward=False,
+    reward_token_id=-1,
+    num_proc=32,
+    use_varlen_attn=use_varlen_attn,
+    shuffle_before_pack=True,
+)
+
+train_dataloader = dict(
+    batch_size=batch_size,
+    num_workers=dataloader_num_workers,
+    dataset=train_dataset,
+    sampler=dict(type=sampler, shuffle=True),
+    collate_fn=dict(
+        type=preference_collate_fn, use_varlen_attn=use_varlen_attn))
+
+
+#######################################################################
+#                    PART 4  Scheduler & Optimizer                    #
+#######################################################################
+# optimizer
+optim_wrapper = dict(
+    type=AmpOptimWrapper,
+    optimizer=dict(
+        type=optim_type, lr=lr, betas=betas, weight_decay=weight_decay),
+    clip_grad=dict(max_norm=max_norm, error_if_nonfinite=False),
+    accumulative_counts=accumulative_counts,
+    loss_scale='dynamic',
+    dtype='float16')
+
+# learning policy
+# More information: https://github.com/open-mmlab/mmengine/blob/main/docs/en/tutorials/param_scheduler.md  # noqa: E501
+param_scheduler = [
+    dict(
+        type=LinearLR,
+        start_factor=1e-5,
+        by_epoch=True,
+        begin=0,
+        end=warmup_ratio * max_epochs,
+        convert_to_iter_based=True),
+    dict(
+        type=CosineAnnealingLR,
+        eta_min=0.0,
+        by_epoch=True,
+        begin=warmup_ratio * max_epochs,
+        end=max_epochs,
+        convert_to_iter_based=True)
+]
+
+# train, val, test setting
+train_cfg = dict(type=TrainLoop, max_epochs=max_epochs)
+
+#######################################################################
+#                           PART 5  Runtime                           #
+#######################################################################
+# Log the dialogue periodically during the training process, optional
+custom_hooks = [
+    dict(type=DatasetInfoHook, tokenizer=tokenizer),
+    # dict(
+    #     type=EvaluateChatHook,
+    #     tokenizer=tokenizer,
+    #     every_n_iters=evaluation_freq,
+    #     evaluation_inputs=evaluation_inputs,
+    #     system=SYSTEM,
+    #     prompt_template=prompt_template),
+    dict(type=HFCheckpointHook,
+        out_dir=out_dir)
+]
+
+if use_varlen_attn:
+    custom_hooks += [dict(type=VarlenAttnArgsToMessageHubHook)]
+
+# configure default hooks
+default_hooks = dict(
+    # record the time of every iteration.
+    timer=dict(type=IterTimerHook),
+    # print log every 10 iterations.
+    logger=dict(type=LoggerHook, log_metric_by_epoch=False, interval=10),
+    # enable the parameter scheduler.
+    param_scheduler=dict(type=ParamSchedulerHook),
+    # save checkpoint per `save_steps`.
+    checkpoint=dict(
+        type=CheckpointHook,
+        by_epoch=False,
+        interval=save_steps,
+        save_optimizer=False,
+        out_dir=out_dir,
+        save_last=False,
+        save_param_scheduler=False,
+        max_keep_ckpts=save_total_limit),
+    # set sampler seed in distributed evrionment.
+    sampler_seed=dict(type=DistSamplerSeedHook),
+)
+
+# configure environment
+env_cfg = dict(
+    # whether to enable cudnn benchmark
+    cudnn_benchmark=False,
+    # set multi process parameters
+    mp_cfg=dict(mp_start_method='fork', opencv_num_threads=0),
+    # set distributed parameters
+    dist_cfg=dict(backend='nccl'),
+)
+
+# set visualizer
+visualizer = dict(type=Visualizer, vis_backends=[dict(type=WandbVisBackend)])
+
+# set log level
+log_level = 'INFO'
+
+# load from which checkpoint
+load_from = None
+
+# whether to resume training from the loaded checkpoint
+resume = False
+
+# Defaults to use random seed and disable `deterministic`
+randomness = dict(seed=None, deterministic=False)
+
+# set log processor
+log_processor = dict(by_epoch=False)
